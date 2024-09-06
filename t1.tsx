a3fde447d18864172d5a0009313edda27d31bcb9
AnthonyTrinh <hunganh.trinh@totalticketing.com>
2024-09-06 16:21:40 +0700

    map

---
+++ t1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="t1" tilewidth="24" tileheight="24" tilecount="79" columns="1">
+ <image source="img/t1.png" width="24" height="1896"/>
+</tileset>
